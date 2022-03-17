c9083c5b63da5177a86401991e3524d39efceb6c
codingbuff <jon.s.gordon@colorado.edu>
2022-03-17 02:09:30 -0600

    progress for iteration since this is the deadline

---
+++ ChordJam/ChordJam/app/src/main/java/com/example/chordjam/MainActivity.kt
@@ -0,0 +1,194 @@
+package com.example.chordjam
+
+import android.app.AlertDialog
+import android.content.DialogInterface
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.ImageView
+import android.widget.Spinner
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+import kotlin.properties.Delegates
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var topText: TextView
+    lateinit var nextChordImg: ImageView
+    lateinit var chord1Img: ImageView
+    lateinit var chord2Img: ImageView
+    lateinit var chord3Img: ImageView
+    lateinit var chord4Img: ImageView
+    lateinit var addChordKey: String
+    lateinit var addChordType: String
+    lateinit var editChordKey: String
+    lateinit var editChordType: String
+    lateinit var newChord: String
+    lateinit var editChord: String
+    val CAPACITY = 4
+    lateinit var chordProgression: MutableList<ImageView>
+    var nextChordIdx by Delegates.notNull<Int>()
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.save -> {
+                openSaveDialog()
+                return true
+            }
+            R.id.load -> {
+                Toast.makeText(this,"you clicked Load",Toast.LENGTH_LONG).show()
+                return true
+            } else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun saveProgression(chordProgression: MutableList<ImageView>):Boolean {
+        //TODO: implement saveProgression
+        //stores currently displayed chord progression for later use
+        //will require an edit text view and transferring of data
+        return true
+    }
+
+    private fun openSaveDialog() {
+        val saveBuilder = AlertDialog.Builder(this@MainActivity)
+        saveBuilder.apply {
+            setPositiveButton(R.string.save) {
+                    _, _ ->
+                var progressionSaved = saveProgression(chordProgression)
+                val snackbar =
+                    Snackbar.make(getWindow().getDecorView().getRootView(),
+                        if (progressionSaved) R.string.saveSuccess else R.string.saveFail,
+                        Snackbar.LENGTH_LONG
+                    )
+                snackbar.show()
+            }
+            setNegativeButton(R.string.cancel,
+                DialogInterface.OnClickListener { dialog, id ->
+                    // User cancelled the dialog
+                })
+        }
+        saveBuilder.setMessage(R.string.saveDialogMsg)
+            .setTitle(R.string.saveDialogTitle)
+        val saveDialog = saveBuilder.create()
+        saveDialog.show()
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        topText = findViewById<TextView>(R.id.instructionTextView)
+        chord1Img = findViewById<ImageView>(R.id.chord1)
+        chord2Img = findViewById<ImageView>(R.id.chord2)
+        chord3Img = findViewById<ImageView>(R.id.chord3)
+        chord4Img = findViewById<ImageView>(R.id.chord4)
+        nextChordIdx = 0
+        chordProgression = listOf(chord1Img,chord2Img,chord3Img,chord4Img).toMutableList()
+        newChord = ""
+        nextChordImg = chordProgression[nextChordIdx]
+        val fab: View = findViewById(R.id.addChordFab)
+        fab.setOnClickListener { view ->
+            if(nextChordIdx >= CAPACITY){
+                val snackbar =
+                    Snackbar.make(view, "capacity reached", Snackbar.LENGTH_LONG)
+                snackbar.show()
+            }
+            else{
+                openAddDialog(nextChordImg)
+            }
+        }
+    }
+
+    fun getImageString(key: String, type: String): String {
+        var returnString = ""
+        if (key.length == 1){
+            returnString = key.lowercase() + type
+        }
+        else {
+            returnString = key[0].lowercase() + "sharp" + key[3].lowercase() + "flat" + type
+        }
+        return returnString
+    }
+
+    fun createNewChordImage(){
+
+        newChord = getImageString(addChordKey,addChordType)
+
+        val resId = resources.getIdentifier(newChord, "drawable", packageName)
+        nextChordImg.setImageResource(resId)
+        var newImg = nextChordImg
+        nextChordImg.setOnClickListener{openEditDialog(newImg) }
+
+        if (nextChordIdx == 0){
+            topText.visibility = View.INVISIBLE
+        }
+
+        nextChordIdx += 1
+        if(nextChordIdx < CAPACITY){
+            nextChordImg = chordProgression[nextChordIdx]
+        }
+    }
+
+    private fun editChordImage(editImg: ImageView) {
+        editChord = getImageString(editChordKey,editChordType)
+        val resId = resources.getIdentifier(editChord, "drawable", packageName)
+        editImg.setImageResource(resId)
+    }
+
+    private fun openEditDialog(editImg: ImageView) {
+        val editChordDialogView = View.inflate(this@MainActivity, R.layout.select_chord_dialog, null)
+        val editChordBuilder = AlertDialog.Builder(this@MainActivity)
+        editChordBuilder.setView(editChordDialogView)
+        editChordBuilder.apply {
+            setPositiveButton(R.string.apply){
+                    _, _->
+                val editKeySpinner = editChordDialogView.findViewById<Spinner>(R.id.addKeySpinner)
+                val editTypeSpinner = editChordDialogView.findViewById<Spinner>(R.id.addTypeSpinner)
+                editChordKey = editKeySpinner.selectedItem.toString()
+                editChordType = editTypeSpinner.selectedItem.toString()
+                editChordImage(editImg)
+            }
+            setNegativeButton(R.string.cancel,
+                DialogInterface.OnClickListener { dialog, id ->
+                    // User cancelled the dialog
+                })
+        }
+        editChordBuilder.setMessage(R.string.addChordDialogMsg)
+            .setTitle(R.string.editChordDialogTitle)
+
+        val editChordDialog = editChordBuilder.create()
+        editChordDialog.show()
+    }
+
+    fun openAddDialog(nextChord: ImageView){
+        val addChordDialogView = View.inflate(this@MainActivity, R.layout.select_chord_dialog, null)
+        val addChordBuilder = AlertDialog.Builder(this@MainActivity)
+        addChordBuilder.setView(addChordDialogView)
+        addChordBuilder.apply {
+            setPositiveButton(R.string.add){
+                _, _->
+                val addKeySpinner = addChordDialogView.findViewById<Spinner>(R.id.addKeySpinner)
+                val addTypeSpinner = addChordDialogView.findViewById<Spinner>(R.id.addTypeSpinner)
+                addChordKey = addKeySpinner.selectedItem.toString()
+                addChordType = addTypeSpinner.selectedItem.toString()
+                createNewChordImage()
+            }
+            setNegativeButton(R.string.cancel,
+                DialogInterface.OnClickListener { dialog, id ->
+                    // User cancelled the dialog
+                })
+        }
+        addChordBuilder.setMessage(R.string.addChordDialogMsg)
+            .setTitle(R.string.addChordDialogTitle)
+        val addChordDialog = addChordBuilder.create()
+        addChordDialog.show()
+    }
+}
