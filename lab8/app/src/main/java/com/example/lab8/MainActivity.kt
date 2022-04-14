90f2a1e12cf8af5b995a97590aa1c689be9a50e7
codingbuff <jon.s.gordon@colorado.edu>
2022-04-14 13:27:58 -0600

    api kinda works

--- lab8/app/src/main/java/com/example/lab8/MainActivity.kt
+++ lab8/app/src/main/java/com/example/lab8/MainActivity.kt
@@ -1,47 +1,37 @@
 package com.example.lab8
 
-import android.content.Context
-import android.graphics.Movie
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.JsonToken
 import android.util.Log
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import androidx.activity.viewModels
-import com.android.volley.Request
-import com.android.volley.Response
-import com.android.volley.toolbox.StringRequest
-import com.android.volley.toolbox.Volley
-import com.example.lab8.model.Word
+import androidx.appcompat.app.AppCompatActivity
 import com.example.lab8.model.WordViewModel
 import com.example.lab8.util.JSONdata
-import org.json.JSONException
-import org.json.JSONObject
-import org.json.JSONTokener
 
 
 class MainActivity : AppCompatActivity() {
     private val viewModel: WordViewModel by viewModels()
-    private val btn: Button
-    val lookUpView = findViewById<EditText>(R.id.editWord)
+    lateinit var lookUpView: EditText
+    lateinit var submitBtn: Button
+    lateinit var searchWord: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val submitBtn = findViewById<Button>(R.id.submitBtn)
-        val loader = JSONdata()
+        lookUpView = findViewById<EditText>(R.id.editWord)
+        submitBtn = findViewById<Button>(R.id.submitBtn)
+
         var searchWord = ""
-        setContentView(R.layout.activity_main)
+
+        submitBtn.setOnClickListener {
+            searchWord = lookUpView.text.toString()
+            Log.d("setOnClickListener","searchWord: ${searchWord}")
+            val loader = JSONdata()
+            loader.loadJSON(this.applicationContext,viewModel,searchWord)
+        }
     }
 
-
-    fun getDef(view: View) {
-        searchWord = lookUpView.toString()
-        Log.d("click listener", "${searchWord}")
-        //loader.loadJSON(this.applicationContext, viewModel, searchWord)
-    }
 
 
 }
